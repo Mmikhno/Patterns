f0e024ceba7fce443ca8e6734ce9fad3c1743f9d
Maria Mikhno <mmikhno89@gmail.com>
2022-10-27 19:54:36 +0300

    Test

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,7 +2,6 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -14,26 +13,35 @@ public class DataGenerator {
     }
 
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return date;
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
+    public static String generateCity() {
+        String str =
+                "Магас,Йошкар-Ола,Иваново,Челябинск,Майкоп,Горно-Алтайск,Уфа,Улан-Удэ,Махачкала,Нальчик,Элиста,Черкесск,"
+                        + "Петрозаводск,Сыктывкар,Симферополь,Саранск,Якутск,Владикавказ,Казань,Кызыл,Ижевск,Абакан,Грозный,Чебоксары,Барнаул,Чита,Петропавловск-Камчатский,Краснодар,"
+                        + "Красноярск,Пермь,Владивосток,Ставрополь,Хабаровск,Благовещенск,Архангельск,Астрахань,Белгород,Брянск,Владимир,"
+                        + "Волгоград,Вологда,Воронеж,Иркутск,Калининград,Калуга,Кемерово,Киров,Кострома,Курган,Курск,Санкт-Петербург,Липецк,"
+                        + "Магадан,Красногорск,Мурманск,Нижний Новгород,Великий Новгород,Новосибирск,Омск,Оренбург,Орёл,Пенза,Псков,"
+                        + "Ростов-на-Дону,Рязань,Самара,Саратов,Южно-Сахалинск,Екатеринбург,Смоленск,Тамбов,Тверь,Томск,Тула,"
+                        + "Тюмень,Ульяновск,Ярославль,Москва,Санкт-Петербург,Севастополь,Биробиджан,Нарьян-Мар,Ханты-Мансийск,Анадырь,Салехард";
+        String[] arrayOfCity;
+        arrayOfCity = str.split(",");
+        int rnd = new Random().nextInt(arrayOfCity.length);
+        String city = arrayOfCity[rnd];
         return city;
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
         return name;
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
         return phone;
     }
 
@@ -42,8 +50,7 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
+            UserInfo user = new UserInfo(DataGenerator.generateCity(), DataGenerator.generateName(locale), DataGenerator.generatePhone(locale));
             return user;
         }
     }
@@ -54,4 +61,6 @@ public class DataGenerator {
         String name;
         String phone;
     }
+
+
 }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,11 +1,16 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
@@ -22,10 +27,118 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        SelenideElement dateInput = $("[data-test-id='date'] input");
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(firstMeetingDate);
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $$("[data-test-id='agreement']").first().click();
+        $$("button").find(exactText("Запланировать")).click();
+        SelenideElement notification = $("[data-test-id='success-notification']");
+        notification.shouldBe(visible, Duration.ofMillis(15000));
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(secondMeetingDate);
+        $$("button").find(exactText("Запланировать")).click();
+        SelenideElement replanNotification = $("[data-test-id='replan-notification']");
+        replanNotification.shouldBe(visible, Duration.ofMillis(15000));
+        replanNotification.$(".notification__title").shouldHave(exactText("Необходимо подтверждение"));
+        replanNotification.$(".notification__content").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $$("button").find(exactText("Перепланировать")).click();
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
+
+    @Test
+    void shouldSuccessfulPlanMeetingWhenCityChanged() {
+        //изменить город вместе с датой
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        SelenideElement city = $("[data-test-id='city'] input");
+        city.setValue(validUser.getCity());
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        SelenideElement dateInput = $("[data-test-id='date'] input");
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(firstMeetingDate);//дата, установленная для первой встречи
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $$("[data-test-id='agreement']").first().click();
+        $$("button").find(exactText("Запланировать")).click();
+        SelenideElement notification = $("[data-test-id='success-notification']");
+        notification.shouldBe(visible, Duration.ofMillis(15000));
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(secondMeetingDate);
+        city.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        city.setValue(DataGenerator.generateCity());
+        $$("button").find(exactText("Запланировать")).click();
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+    @Test
+    void shouldSuccessfulPlanMeetingWhenNameChanged() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 5;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        SelenideElement city = $("[data-test-id='city'] input");
+        city.setValue(validUser.getCity());
+        SelenideElement name = $("[data-test-id='name'] input");
+        name.setValue(validUser.getName());
+        SelenideElement dateInput = $("[data-test-id='date'] input");
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(firstMeetingDate);
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $$("[data-test-id='agreement']").first().click();
+        $$("button").find(exactText("Запланировать")).click();
+        SelenideElement notification = $("[data-test-id='success-notification']");
+        notification.shouldBe(visible, Duration.ofMillis(15000));
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(secondMeetingDate);
+        name.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        name.setValue(DataGenerator.generateName("ru"));
+        $$("button").find(exactText("Запланировать")).click();
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+
+    }
+
+    @Test
+    void shouldSuccessfulPlanMeetingWhenPhoneChanged() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 5;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        SelenideElement dateInput = $("[data-test-id='date'] input");
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(firstMeetingDate);
+        SelenideElement phone = $("[data-test-id='phone'] input");
+        phone.setValue(validUser.getPhone());
+        $$("[data-test-id='agreement']").first().click();
+        $$("button").find(exactText("Запланировать")).click();
+        SelenideElement notification = $("[data-test-id='success-notification']");
+        notification.shouldBe(visible, Duration.ofMillis(15000));
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        dateInput.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        dateInput.setValue(secondMeetingDate);
+        phone.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        phone.setValue(DataGenerator.generatePhone("ru"));
+        $$("button").find(exactText("Запланировать")).click();
+        notification.$(".notification__title").shouldHave(exactText("Успешно!"));
+        notification.$(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
 }
